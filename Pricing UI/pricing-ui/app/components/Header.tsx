a3610d461b53513125c1c6c94f2d803c0a2f2527
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2023-01-16 20:39:18 +0500

    create response for mobile view

--- Pricing UI/pricing-ui/app/components/Header.tsx
+++ Pricing UI/pricing-ui/app/components/Header.tsx
@@ -13,10 +13,10 @@ export function Header() {
           px="32px"
           textAlign="center"
         >
-          <Heading fontWeight="800" fontSize="48px">
+          <Heading fontWeight="800" fontSize={['3xl','3xl','5xl']} textAlign={['left','left','center']}>
             Simple pricing for your business
           </Heading>
-          <Text fontWeight="500" fontSize="24px" pt="16px">
+          <Text fontWeight="500" fontSize={['lg','lg','2xl']} pt="16px" textAlign={['left','left','center']}>
             Plans that are carefully crafted to suit your business.
           </Text>
         </Box>
--- Pricing UI/pricing-ui/app/components/features.tsx
+++ Pricing UI/pricing-ui/app/components/features.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Icon,StackProps,Text } from "@chakra-ui/react";
+import { Box, HStack, Icon,StackProps,Text,Flex, VStack } from "@chakra-ui/react";
 import { ElementType } from "react";
 import { MoneyBackGuaranteeIcon, NoSetupFeeIcon,MonthlySubIcon } from "./icons/icons";
 
@@ -9,8 +9,8 @@ interface FeatureProps extends StackProps{
 function Feature(props: FeatureProps){
     const {icon,children, ...rest}= props;
     return(
-        <HStack {...rest} spacing='24px'>
-            <Icon as={icon} boxSize='48px'>
+        <HStack {...rest} spacing={['48px','48px','24px']} >
+            <Icon as={icon} boxSize={['32px','32px','48px']}>
             </Icon>
             <Text textAlign={"left"} fontSize={"18px"} fontWeight={"700"}> {children}</Text>
         </HStack>
@@ -19,8 +19,12 @@ function Feature(props: FeatureProps){
 
 export function Features(){
     return (
-      <Box maxW={"1024px"} m="auto" pt="60px" pb="32px">
-        <HStack px="48px" spacing={"20px"}>
+    
+      <Box maxW={"1024px"} m="auto" pt="60px" pb="32px" >
+        
+        <HStack px="48px" spacing={"20px"} >
+            <VStack spacing={4} >
+        <Flex direction={['column','column','row']}>
           <Feature icon={MoneyBackGuaranteeIcon}>
             30 days money back Guarantee
           </Feature>
@@ -30,7 +34,11 @@ export function Features(){
           <Feature icon={MonthlySubIcon}>
             No monthly subscription Pay once and for all
           </Feature>
+         
+      </Flex>
+      </VStack>
+
         </HStack>
-      </Box>
+       </Box>
     );
 }
